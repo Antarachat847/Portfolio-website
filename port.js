31660bc2c73fa9e929457458544eae693ce62a3e
Antarachat847 <chata416@newschool.edu>
2025-01-10 20:30:26 +0530

    HAHA

--- port.js
+++ port.js
@@ -6,13 +6,4 @@ const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        // Add the 'active' class when the card is in view
-        entry.target.classList.add('active');
-      }
-    });
-  },
-  { threshold: 0.5 } // Trigger when 50% of the card is visible
-);
-
-// Observe each card
-cards.forEach((card) => observer.observe(card));
+        // Add
